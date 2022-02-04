4850dfdc003838c45d79e820328744ff2545cfe8
Reconix <Reconix@users.noreply.github.com>
2022-02-04 23:29:17 +0000

    Blank Template
    A blank starter template that's blank xD

---
+++ inc/js-scripts.php
@@ -0,0 +1,6 @@
+    <!-- jQuery 2.1.3 -->
+    <script src="assets/components/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 3.3.2 JS -->
+    <script src="assets/components/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- App 0.1 -->
+    <script src="assets/js/app.js" type="text/javascript"></script>
